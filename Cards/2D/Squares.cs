eb1ed3ad6c17b0c6639d47b9c5b5fffca68c3582
Thomas Roberts <troberts@brainstorminc.com>
2024-04-18 13:28:22 -0600

    Squares set up

--- Cards/2D/Squares.cs
+++ Cards/2D/Squares.cs
@@ -3,5 +3,24 @@ using System;
 
 public partial class Squares : Control
 {
-  
+  [Export]
+  public Color affectedColor = new Color(0.765f, 0.725f, 0.388f); 
+
+  private ColorRect[] tiles = new ColorRect[25];
+
+  public override void _Ready()
+  {
+    for (int i = 0; i < 25; i++)
+    {
+      tiles[i] = GetNode<ColorRect>("Tile" + i.ToString());
+    }
+  }
+
+  public void ColorTiles(int[] tilesToColor)
+  {
+    for (int i = 0; i < tilesToColor.Length; i++)
+    {
+      tiles[tilesToColor[i]].Color = affectedColor;
+    }
+  }
 }
