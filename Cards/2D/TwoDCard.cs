ebea0d6ae4e3a53be0c1f98c7401a9bbf858afa2
Thomas Roberts <troberts@brainstorminc.com>
2024-04-23 13:21:44 -0600

    Init card properties

--- Cards/2D/TwoDCard.cs
+++ Cards/2D/TwoDCard.cs
@@ -6,6 +6,7 @@ public partial class TwoDCard : Control
   [Export]
   private CardData cardData {get; set;}
   private TextureRect[] pawns = new TextureRect[3];
+  private Label pointLabel;
 
   public override void _Ready()
   {
@@ -13,6 +14,15 @@ public partial class TwoDCard : Control
     pawns[1] = GetNode<TextureRect>("Pawns/Pawn2");
     pawns[2] = GetNode<TextureRect>("Pawns/Pawn3");
 
+    pointLabel = GetNode<Label>("PointLabel");
+  }
 
+  private void SetInitialCardProperties()
+  {
+    pointLabel.Text = cardData.CardValue.ToString();
+    for (int i = 0; i < pawns.Length; i++)
+    {
+      pawns[i].Visible = i < cardData.CardCost;
+    }
   }
 }
