db0523fd94c473fdd431c8c16877f3eb8db6b33a
= <=>
2024-05-06 09:51:47 -0600

    Styling tweaks

---
+++ Cards/3D/ThreeDCard.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public partial class ThreeDCard : Node3D
+{
+  [Export]
+  private CardData cardData {get; set;}
+
+  private void SetInitialCardProperties()
+  {
+    
+  }
+}
